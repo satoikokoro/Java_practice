4a32e60aba1ed4f8145013bf6794d8754f16a09e
EC2 Default User <130027255+satoikokoro@users.noreply.github.com>
2023-08-25 12:53:45 +0000

    230825

---
+++ Main1.java
@@ -0,0 +1,9 @@
+public class Main1{
+ public static void main(String[] args){
+   System.out.println("31 + 31の計算をします");
+   System.out.println(31+31);
+   System.out.println(35 - 10);
+   System.out.println(-5 * 2);
+   System.out.println(6 * 6 * 3.14);
+ }
+}
