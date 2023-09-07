d49034546be149b7706d0034c36d8edb68612b86
EC2 Default User <130027255+satoikokoro@users.noreply.github.com>
2023-09-07 13:00:02 +0000

    230907

---
+++ Main9.java
@@ -0,0 +1,9 @@
+public class Main9 {
+  public static void main (String [] args){
+    boolean result = true;
+    char favoriteCharcter = '駆';
+    double pi = 3.14;
+    long number = 314159265853979L;
+    String msg = "ミナトの攻撃!敵に15ポイントのダメージを与えた。";
+  }
+}
